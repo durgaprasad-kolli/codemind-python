52fa36033e8dba03b47263a2f3c29c335a807b88
durgaprasad-kolli <119840335+durgaprasad-kolli@users.noreply.github.com>
2023-05-23 12:14:48 +0530

    def FactorSum(n):     s = 0     for i in range(1, n) :         if n%i == 0 :             s+=i     return s
    a = int(input())
    b = int(input())
    x = FactorSum(a)
    y = FactorSum(b)
    if x==b and y == a :
        print("Amicable")
    else :
        print("Not Amicable")

--- Amicable_Numbers.py
+++ Amicable_Numbers.py
@@ -1,15 +1,15 @@
+def FactorSum(n):
+    s = 0
+    for i in range(1, n) :
+        if n%i == 0 :
+            s+=i
+    return s
+    
 a = int(input())
 b = int(input())
-sum=0
-count=0
-for i in range(1,a-1):
-    if a%i==0:
-        sum+=i
-for j in range(1,b-1):
-    if b%j==0:
-        count+=j
-if count==a and sum==b:
+x = FactorSum(a)
+y = FactorSum(b)
+if x==b and y == a :
     print("Amicable")
-else:
+else :
     print("Not Amicable")
-        
