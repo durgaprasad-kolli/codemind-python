6e207de34eecd18d2785953a978def28dfd9680b
durgaprasad-kolli <119840335+durgaprasad-kolli@users.noreply.github.com>
2023-05-23 12:10:46 +0530

    def rev(a):     n = abs(a)     rev = 0     while n != 0:         rev = (rev*10) + (n%10)         n//=10     return rev num = int(input()) res = rev(num) if num < 0:     print(int(-1*res)) else:     print(res)

--- Reverse_Integer.py
+++ Reverse_Integer.py
@@ -1,15 +1,13 @@
-a = input()
-b = a[::-1]
-k=[]
-for i in b:
-    k.append(i)
-for j in range(0,len(k)):
-    if k[0]=="0":
-        k.remove(k[0])
-    elif k[-1]=="-":
-        k.remove(k[-1])
-        k.insert(0,"-")
-for l in k:
-    print(l,end='')
-    
-    
+def rev(a):
+    n = abs(a)
+    rev = 0
+    while n != 0:
+        rev = (rev*10) + (n%10)
+        n//=10
+    return rev
+num = int(input())
+res = rev(num)
+if num < 0:
+    print(int(-1*res))
+else:
+    print(res)
