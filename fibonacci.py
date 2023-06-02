0f005c6f6902c2100f0737432376f908a469982c
durgaprasad-kolli <119840335+durgaprasad-kolli@users.noreply.github.com>
2023-06-02 16:20:39 +0530

    n = int(input()) a, b = 0, 1 for i in range(n):     print(a, end = " ")     c = a + b     a, b = b, c

--- fibonacci.py
+++ fibonacci.py
@@ -1,10 +1,6 @@
-def fibonacci(n):
-  a, b = 0, 1
-  result = []
-  for i in range(n):
-    result.append(a)
-    a, b = b, a+b
-  for j in result:
-      print(j,end=" ")
-a=int(input())
-fibonacci(a)
+n = int(input())
+a, b = 0, 1
+for i in range(n):
+    print(a, end = " ")
+    c = a + b
+    a, b = b, c
