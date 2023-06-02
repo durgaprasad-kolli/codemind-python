d660af864a8ded5eddcb8726cc1d189813154cb9
durgaprasad-kolli <119840335+durgaprasad-kolli@users.noreply.github.com>
2023-06-02 16:14:49 +0530

    def n_rev(n):     rev = 0     while n > 0:         rev = (rev*10) + (n%10)         n//=10     return rev
    num = int(input())
    num_sq = num*num
    rev_num = n_rev(num)
    new = rev_num*rev_num
    final = n_rev(new)
    if(num_sq == final):
        print("True")
    else :
        print("False")

--- Adam_number.py
+++ Adam_number.py
@@ -1,12 +1,16 @@
-a = input()
-b = a[::-1]
-c = int(a)
-d = c*c
-e = int(b)
-f = e*e
-g = str(f)[::-1]
-h = str(d)
-if(g==h):
+def n_rev(n):
+    rev = 0
+    while n > 0:
+        rev = (rev*10) + (n%10)
+        n//=10
+    return rev
+    
+num = int(input())
+num_sq = num*num
+rev_num = n_rev(num)
+new = rev_num*rev_num
+final = n_rev(new)
+if(num_sq == final):
     print("True")
-else:
+else :
     print("False")
